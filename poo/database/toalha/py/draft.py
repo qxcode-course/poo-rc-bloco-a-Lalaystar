e5626240380c826c94999dd18f882b3867db661b
Lalaystar <layannemenezes78@gmail.com>
2025-09-23 14:36:34 +0000

    poo2309

---
+++ poo/database/toalha/py/draft.py
@@ -0,0 +1,44 @@
+class Towel:
+    def __init__(self,color:str,size:str):
+        self.color:str=color
+        self.size:str=size
+        self.wetness:int=0
+
+    
+
+    def dry(self, amount: int)->None:
+        self.wetness+=amount
+        if self.wetness>=self.isMaxWetness():
+            self.wetness=self.isMaxWetness()
+            print("toalha ta ensopada ze")
+
+    def isMaxWetness(self)->int:
+        if self.size=="P":
+            return 10
+        if self.size=="M":
+            return 20
+        if self.size=="G":
+            return 30
+        return 0
+        
+    def __str__(self)->str:
+        return f"Color:{self.color}, Size:{self.size}, Wet:{self.wetness}"
+
+def main():
+    towel:Towel=Towel("","")
+    while True:
+        line: str=input()
+        args:list[str]=line.split(" ")
+
+        if args[0]=="end":
+            break
+        elif args[0]=="new":
+            color:str=args[1]
+            size:str=args[2]
+            towel=Towel(color,size)
+        elif args[0]=="show":
+            print(towel)
+        else:
+            print("fail: command not found")
+
+main()

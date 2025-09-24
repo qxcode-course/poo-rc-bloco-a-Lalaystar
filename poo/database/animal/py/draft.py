d08c34f80968466d15eff819c76475846078a7f7
Layanne <layannemenezes78@gmail.com>
2025-09-24 19:35:09 -0300

    AttAnimal

--- poo/database/animal/py/draft.py
+++ poo/database/animal/py/draft.py
@@ -1,15 +1,23 @@
-class animal:
-    def __init__(self, specie:str, age:int, sound:str):
+class Animal:
+    def __init__(self, specie:str, sound:str):
         self.specie: str=specie
-        self.age:int=0
         self.sound: str=sound
-    
+        self.age: int=0
+
     def __str__(self) -> str:
         return f"{self.specie}:{self.age}:{self.sound}"
+    
+
 def main():
-    animal: Animal = Animal("",0,"")
+    animal: Animal = Animal("","")
     while True:
         line: str=input()
         print("$"+line)
-    
+        args:list[str]=line.split(" ")
+
+        if args[0]=="end":
+            break
+        elif args[0]=="show":
+            print(animal)
+       
 main()

b2364eea09d5a833dc12de2c3bde767f680ab7f9
Layanne <layannemenezes78@gmail.com>
2025-10-01 21:39:42 -0300

    animalfim

--- poo/database/animal/py/draft.py
+++ poo/database/animal/py/draft.py
@@ -1,14 +1,34 @@
 class Animal:
-    def __init__(self, specie:str, sound:str):
+    def __init__(self, specie:str, sound:str): #aq o construtor bixa
         self.specie: str=specie
         self.sound: str=sound
         self.age: int=0
 
-    def __str__(self) -> str:
+    def __str__(self) -> str: #aq o toString bixa
         return f"{self.specie}:{self.age}:{self.sound}"
     
-
-def main():
+    def ageBy(self, increment:int)->None:
+        if self.age==4:
+            print(f"warning: {self.specie} morreu")
+            return 
+        self.age+=increment
+        if self.age>=4:
+            self.age=4
+            print(f"warning: {self.specie} morreu")
+    
+    def makeSound(self)->str: #type:ignore 
+        if self.age==0:
+            return "---"
+        if self.age==1:
+            return self.sound
+        if self.age==2:
+            return self.sound
+        if self.age==3:
+            return self.sound
+        if self.age==4:
+            return "RIP"
+        
+def main(): #aq acontece a interaçao com o código
     animal: Animal = Animal("","")
     while True:
         line: str=input()
@@ -17,7 +37,15 @@ def main():
 
         if args[0]=="end":
             break
-        elif args[0]=="show":
+        if args[0]=="show":
             print(animal)
-       
+        if args[0]=="init":
+            specie:str=args[1]
+            sound:str=args[2]
+            animal=Animal(specie,sound)
+        if args[0]=="grow":
+            increment: int=int(args[1])
+            animal.ageBy(increment)
+        if args[0]=="noise":
+            print(animal.makeSound())
 main()

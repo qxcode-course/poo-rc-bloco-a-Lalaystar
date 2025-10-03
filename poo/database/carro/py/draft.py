b245da734fb4ff9f9268d886471414f55f7fe250
Layanne <layannemenezes78@gmail.com>
2025-10-03 14:36:53 -0300

    AttCar

---
+++ poo/database/carro/py/draft.py
@@ -0,0 +1,22 @@
+class Car:
+    def __init__(self):
+        self.gas:int=0
+        self.km:int=0
+        self.pas:int=0
+
+    def __str__(self)->str:
+        return f"pass: {self.pas}:, gas: {self.gas}, km: {self.km}"
+
+def main():
+    car:Car=Car()
+    while True:
+        line:str=input()
+        print("$"+line)
+        args:list[str]=line.split(" ")
+        
+        if args[0]=="end":
+            break
+        if args[0]=="show":
+            print(car)
+        if args[0]=="init":
+main()
